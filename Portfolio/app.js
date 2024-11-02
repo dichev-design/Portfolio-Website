48981683ce1e0ae60ade9b1e1ae2c4515bb495f6
dichev-design <dichev.official@gmail.com>
2024-11-02 09:58:03 +0200

    Delete Portfolio directory

--- Portfolio/app.js
+++
@@ -1,8 +0,0 @@
-const hamMenu = document.querySelector('.ham-menu');
-
-const offScreenMenu = document.querySelector('.off-screen_menu');
-
-hamMenu.addEventListener('click', () =>{
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
-})
